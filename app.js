06e810ba3ef60906f31f6009d1ed17407fd147bc
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-03 16:14:12 +0530

    script type updated to application/json

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const html = `
     <meta charset="utf-8" />
     <title>Custom Journey Builder Activity</title>
 
-    <script  src="js/jquery.min.js"></script>
-    <script  src="js/require.js"></script>
-    <script  src = "js/postmonger.js"></script>
+    <script type="application/json"  src="js/jquery.min.js"></script>
+    <script type="application/json"  src="js/require.js"></script>
+    <script type="application/json"  src = "js/postmonger.js"></script>
 
     
   </head>
