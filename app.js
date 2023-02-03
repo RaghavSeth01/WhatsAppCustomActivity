1b1d852c5141b477f1b420c53642f6fe5cda3452
Raghav Seth <raghav.s@techmatrixconsulting.com>
2023-02-03 16:54:41 +0530

    new

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const html = `
     <script type="application/json"  src="js/jquery.min.js"></script>
     <script type="application/json"  src="js/require.js"></script>
     <script type="application/json"  src = "js/postmonger.js"></script>
-    <script   src = "js/activity.js"></script>
+    <script  type="application/json"  src = "js/activity.js"></script>
 
     
   </head>
